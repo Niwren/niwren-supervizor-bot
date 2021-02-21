acbca32c5fb3bf9d8c8fe70a1bb8c3c34cab8fab
Niwren <69695875+Niwren@users.noreply.github.com>
2021-02-21 17:42:00 +0300

    Update yetkilises.js

--- commands/yetkilises.js
+++ commands/yetkilises.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "ses",
     run: async(client, message, args) => {
         let embed = new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp().setThumbnail(message.author.avatarURL).setFooter('🎄Developed by Niwren🎄');
-        if (!client.config.üstYönetim.some(id => message.member.roles.cache.has(id))) {
+        if (!client.config.üstYönetim.some(id => message.member.roles.cache.has(id))&& (!message.member.hasPermission("ADMINISTRATOR"))) {
             return message.channel.send(embed.setDescription("Bu Komut İçin Yetkin Bulunmuyor.")).then(x => x.delete({ timeout: 3000 }))
         }
         let sesteolmayan = message.guild.members.cache.filter(s => s.roles.cache.has('807571845464850452')).filter(s => !s.voice.channel).map(s => s).join(' ')
