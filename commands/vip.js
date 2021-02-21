8137febf3c37050b2cb7f322bb428d0892f00cbc
Niwren <69695875+Niwren@users.noreply.github.com>
2021-02-21 17:41:42 +0300

    Update vip.js

--- commands/vip.js
+++ commands/vip.js
@@ -7,7 +7,7 @@ module.exports = {
     run: async(client, message, args) => {
 
         let embed = new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('#ff0000').setTimestamp().setFooter('Muratva Stark X İngiltereli X Niwren');
-        if (!client.config.mods.some(id => message.member.roles.cache.has(id))) {
+        if (!client.config.mods.some(id => message.member.roles.cache.has(id))&& (!message.member.hasPermission("ADMINISTRATOR"))) {
             return message.channel.send(embed.setDescription("Bu Komut İçin Yetkin Bulunmuyor."))
         }
         //    if (message.channel.id !== client.config.channelID) return message.channel.send(embed.setDescription('Komutu sadece <#${client.config.channelID}> analında kullanabilirsin'))
