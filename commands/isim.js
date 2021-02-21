4dda96f1e066ff85569312b0cfdd805b4c23cead
Niwren <69695875+Niwren@users.noreply.github.com>
2021-02-21 17:30:20 +0300

    Update isim.js

--- commands/isim.js
+++ commands/isim.js
@@ -7,8 +7,8 @@ module.exports = {
     run: async(client, message, args) => {
         let embed = new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('#ff0000').setTimestamp();
 
-        if (!client.config.mods.some(id => message.member.roles.cache.has(id))) {
-            return message.channel.send(embed.setDescription("Komutu kullanan kullanıcıda yetki bulunmamakta!")).then(x => x.delete({ timeout: 5000 }))
+        if (!client.config.mods.some(id => message.member.roles.cache.has(id))&& (!message.member.hasPermission("ADMINISTRATOR"))) {
+            return message.channel.send(embed.setDescription('Komutu kullanan kullanıcıda yetki bulunmamakta!')).then(x => x.delete({ timeout: 5000 }))
         }
 
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
