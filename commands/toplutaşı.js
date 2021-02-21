207c1c3a49b628e41c7aa37f4db4224300d2ad4a
Niwren <69695875+Niwren@users.noreply.github.com>
2021-02-21 17:36:31 +0300

    Update toplutaşı.js

--- commands/toplutaşı.js
+++ commands/toplutaşı.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: "toplutaşı",
     run: async(client, message, args) => {
-        if (!client.config.üstYönetim.some(id => message.member.roles.cache.has(id))) {
+        if (!client.config.üstYönetim.some(id => message.member.roles.cache.has(id))&& (!message.member.hasPermission("ADMINISTRATOR"))) {
             return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setDescription('Komutu kullanan kullanıcıda yetki bulunmamakta!').setColor('#ff0000').setTimestamp().setThumbnail(message.author.avatarURL))
         }
         let channel = args[0];
