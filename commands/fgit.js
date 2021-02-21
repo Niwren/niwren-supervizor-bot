f07d2f2c87c4f7bc487b1f345e5c95e7bd527814
Niwren <69695875+Niwren@users.noreply.github.com>
2021-02-21 17:42:59 +0300

    Update fgit.js

--- commands/fgit.js
+++ commands/fgit.js
@@ -4,6 +4,9 @@ module.exports = {
     name: "forcegit",
     aliases: ["fgit"],
     run: async(client, message, args) => {
+        if (!client.config.üstYönetim.some(id => message.member.roles.cache.has(id))) {
+        return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setDescription('Komutu kullanan kullanıcıda yetki bulunmamakta!').setColor('#ff0000').setTimestamp().setThumbnail(message.author.avatarURL))
+        }
         let embed = new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp();
         if (!message.member.voice.channel) return message.channel.send(embed.setDescription("Önce sesli kanala girmen gerekiyor"))
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
